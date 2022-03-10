c649a0156b472d43961bb01df0adb0fb813c2115
Zer0Cool0 <jlewis0093@gmail.com>
2022-03-10 00:19:06 -0500

    Add project files.

---
+++ RedMQCC/Form1.Designer.vb
@@ -0,0 +1,146 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.BetterFolderBrowser1 = New WK.Libraries.BetterFolderBrowserNS.BetterFolderBrowser(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Transparent
+        Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Agency FB", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(132, 50)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Clear Cache/Start RedM"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.Transparent
+        Me.Button2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button2.Font = New System.Drawing.Font("Agency FB", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(150, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(132, 50)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Clear Cache"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.Color.Transparent
+        Me.Button5.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button5.Font = New System.Drawing.Font("Agency FB", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.Location = New System.Drawing.Point(288, 12)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(132, 50)
+        Me.Button5.TabIndex = 5
+        Me.Button5.Text = "Open RedM Directory"
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.Transparent
+        Me.Button3.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.Font = New System.Drawing.Font("Agency FB", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(426, 12)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(132, 50)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "Settings"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'BetterFolderBrowser1
+        '
+        Me.BetterFolderBrowser1.Multiselect = False
+        Me.BetterFolderBrowser1.RootFolder = "C:\"
+        Me.BetterFolderBrowser1.Title = "Please select your RedM folder..."
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Yu Gothic", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(9, 65)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(142, 14)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "© 2022 |  Zer0Cool0#0001"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Yu Gothic", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(477, 65)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(96, 14)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Version: 1.2 Beta"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(573, 79)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "RedM Easy Clear Cache"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents BetterFolderBrowser1 As WK.Libraries.BetterFolderBrowserNS.BetterFolderBrowser
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ RedMQCC/Form1.vb
@@ -0,0 +1,64 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        If String.IsNullOrEmpty(My.Settings.redm) Then
+            MessageBox.Show("Uh oh! You have not selected your RedM folder! Please do that now! This folder contains 'RedM.exe' and 'RedM Application Data'", "RedM Quick Clear Cache", MessageBoxButtons.OK)
+            If Windows.Forms.DialogResult.OK Then
+                If BetterFolderBrowser1.ShowDialog() = DialogResult.OK Then
+                    My.Settings.redm = BetterFolderBrowser1.SelectedPath
+                End If
+            End If
+        End If
+
+        CenterToScreen()
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim path As String = My.Settings.redm + "\RedM.app\data\server-cache-priv"
+        If MessageBox.Show("Are you sure you want to clear your cache & launch RedM?", "RedM Quick Clear Cache", MessageBoxButtons.YesNo) = Windows.Forms.DialogResult.Yes Then
+            If path <> "" Then
+                Dim fileName As String
+                For Each fileName In Directory.GetFiles(path, "cache_*")
+                    File.Delete(fileName)
+                Next
+            End If
+        End If
+
+        Process.Start(My.Settings.redm + "\RedM.exe")
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Dim path As String = My.Settings.redm + "\RedM.app\data\server-cache-priv"
+        If MessageBox.Show("Are you sure you want to clear your cache? ", "RedM Quick Clear Cache", MessageBoxButtons.YesNo) = Windows.Forms.DialogResult.Yes Then
+            If path <> "" Then
+                Dim fileName As String
+                For Each fileName In Directory.GetFiles(path, "cache_*")
+                    File.Delete(fileName)
+                Next
+            End If
+        End If
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        If BetterFolderBrowser1.ShowDialog() = DialogResult.OK Then
+            My.Settings.redm = BetterFolderBrowser1.SelectedPath
+
+        End If
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+
+        ''Handles Button for opening redm directory''
+        Process.Start(My.Settings.redm)
+
+    End Sub
+End Class
